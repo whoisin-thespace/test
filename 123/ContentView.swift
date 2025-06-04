b3a26de46063322e22db6ac05ada8d3f525e4a0e
SUHO PARK <sowlqdmsso@naver.com>
2025-06-04 11:28:13 +0900

    Initial Commit

---
+++ 123/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  123
+//
+//  Created by SUHO on 6/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 123/_23App.swift
@@ -0,0 +1,17 @@
+//
+//  _23App.swift
+//  123
+//
+//  Created by SUHO on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct _23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
